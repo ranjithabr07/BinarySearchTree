409ac02af687d5e3154e65439aa0587f5961dfbd
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-19 17:42:41 +0530

    Create Binary node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using BinarySearchTree;
+﻿using BinarySearchTree;
 
 Console.WriteLine("Welcome To Binary Search Tree Problem");
 BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
@@ -18,9 +16,3 @@ BinaryTree.AddNode(33);
 BinaryTree.AddNode(16);
 BinaryTree.AddNode(63);
 BinaryTree.AddNode(67);
-
-int Size = BinaryTree.GetSize();
-Console.WriteLine("Size is:" + Size);
-
-bool result = BinaryTree.SearchKey(63);
-Console.WriteLine(result);
