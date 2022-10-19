0de7e25362b42a18a12f29b580503b501c04aca6
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-19 17:34:31 +0530

    Search the Binary tree node

---
+++ BinarySearchTree/BinarySearchTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTreeNode<K>
+    {
+        public K Key;
+        public BinarySearchTreeNode<K> Right;
+        public BinarySearchTreeNode<K> Left;
+
+        public BinarySearchTreeNode(K Key)
+        {
+            this.Key = Key;
+            Right = null;
+            Left = null;
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using BinarySearchTree;
+
+Console.WriteLine("Welcome To Binary Search Tree Problem");
+BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+BinaryTree.AddNode(56);
+BinaryTree.AddNode(30);
+BinaryTree.AddNode(70);
+BinaryTree.AddNode(22);
+BinaryTree.AddNode(40);
+BinaryTree.AddNode(60);
+BinaryTree.AddNode(95);
+BinaryTree.AddNode(11);
+BinaryTree.AddNode(65);
+BinaryTree.AddNode(33);
+BinaryTree.AddNode(16);
+BinaryTree.AddNode(63);
+BinaryTree.AddNode(67);
+
+int Size = BinaryTree.GetSize();
+Console.WriteLine("Size is:" + Size);
+
+bool result = BinaryTree.SearchKey(63);
+Console.WriteLine(result);
